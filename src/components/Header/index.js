cd91d3a7b542f9e4d3d589f85b56d390b71efa75
Guilherme Emerick <emerick.perth@gmail.com>
2024-05-28 16:33:29 -0300

    Menu styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,32 @@
+import logo from "../svg/logo.svg";
+
 const Header = () => {
-  return <h1>Header.js</h1>;
+  const descLogo = "Essa e a logo do Blog";
+  return (
+    <>
+      <header className="flex, flex-space-between">
+        <div className="logo, border">
+          <img src={logo} alt={descLogo} />
+        </div>
+        <div className="search">
+          <input type="text" className="input-search" name="search" />
+        </div>
+        <div>
+          <ul className="menu">
+            <li>
+              <a href="#" className="nav-link">Categories</a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">About</a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
