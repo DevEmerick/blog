6bdc0ce6255489b2a412014e2ae6d938be9c55e9
Guilherme Emerick <emerick.perth@gmail.com>
2024-06-03 20:19:55 -0300

    base config and install axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Header from "./components/Header";
-import Article from "./components/Article";
-
-import { useState } from "react";
+//import Article from "./components/Article";
+import Main from "./Pages/Main"
 
 function App() {
   return (
     <>
-      <Header> </Header>
-      <Article></Article>
+      <Header />
+      <Main />
     </>
   );
 }
