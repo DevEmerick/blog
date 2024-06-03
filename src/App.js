89e9bc84c67cfdf4a18d6a992a283fae009cf584
Guilherme Emerick <emerick.perth@gmail.com>
2024-06-03 17:53:54 -0300

    Removing Count

--- src/App.js
+++ src/App.js
@@ -4,34 +4,10 @@ import Article from "./components/Article";
 import { useState } from "react";
 
 function App() {
-  let [carrinho, setCarrinho] = useState(0);
-
-  const AddItem = () => {
-    setCarrinho(carrinho + 1);
-    console.log(carrinho);
-  };
-
-  const RemoveItem = () => {
-    if (carrinho > 0) {
-      setCarrinho(carrinho - 1);
-    }
-    console.log(carrinho);
-  };
-
   return (
     <>
       <Header> </Header>
       <Article></Article>
-
-      <h1>Carrinho de compras: {carrinho}</h1>
-      <div className="flex">
-        <button className="add-button ml-2" onClick={AddItem}>
-          +
-        </button>
-        <button className="remove-button ml-2" onClick={RemoveItem}>
-          -
-        </button>
-      </div>
     </>
   );
 }
