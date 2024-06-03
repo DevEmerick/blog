3089185032fad667d8871824e1a33a8bbf8b6eb1
Guilherme Emerick <emerick.perth@gmail.com>
2024-06-03 17:46:22 -0300

    React Hooks, UseState & OnClick

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import Header from './components/Header';
-import Article from './components/Article';
+import Header from "./components/Header";
+import Article from "./components/Article";
+
+import { useState } from "react";
 
 function App() {
+  let [carrinho, setCarrinho] = useState(0);
+
+  const AddItem = () => {
+    setCarrinho(carrinho + 1);
+    console.log(carrinho);
+  };
+
+  const RemoveItem = () => {
+    if (carrinho > 0) {
+      setCarrinho(carrinho - 1);
+    }
+    console.log(carrinho);
+  };
+
   return (
     <>
       <Header> </Header>
       <Article></Article>
+
+      <h1>Carrinho de compras: {carrinho}</h1>
+      <div className="flex">
+        <button className="add-button ml-2" onClick={AddItem}>
+          +
+        </button>
+        <button className="remove-button ml-2" onClick={RemoveItem}>
+          -
+        </button>
+      </div>
     </>
   );
 }
