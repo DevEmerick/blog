1d118ab8fadae834298a1ca164e558a7a0ad50d5
Guilherme Emerick <emerick.perth@gmail.com>
2024-06-03 21:34:55 -0300

    listing posts with api

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,12 +1,45 @@
 import api from "../../services/api";
-import Post from "../../components/Post"
+import Post from "../../components/Post";
+import {useState} from 'react';
 
 const Main = () => {
-  return (
+
+    const [post, setPost] = useState([]);
+
+  async function handleAxios() {
+    try {
+        const response = await api.get("/posts");
+        console.log(response.data);
+        setPost(response.data);
+    } 
+    catch (error) {
+        console.log(error);
+    }
+
+  }
+
+  return(
     <>
-      <h1>Main.js</h1>
+    <section className="container">
+        <h1>Mains.Js</h1>
+        <button onClick={handleAxios}>Requisicao</button>
+
+        <div className="mt-5 container-posts">
+            {
+                post.map(post => {
+                    return(
+                        <>
+                        <Post key={post.id} subtitle={post.category} title={post.title}></Post>
+                        </>
+                    )
+                })
+            }
+        </div>
+
+
+    </section>
     </>
-  );
+  )
 };
 
 export default Main;
