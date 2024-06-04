ce01826452bf60cc9957e33e6bdb7c780d295402
Guilherme Emerick <emerick.perth@gmail.com>
2024-06-03 22:05:58 -0300

    React Hook useEffect

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,35 +1,36 @@
 import api from "../../services/api";
 import Post from "../../components/Post";
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 const Main = () => {
 
     const [post, setPost] = useState([]);
 
-  async function handleAxios() {
-    try {
-        const response = await api.get("/posts");
-        console.log(response.data);
-        setPost(response.data);
-    } 
-    catch (error) {
-        console.log(error);
-    }
-
-  }
+    useEffect (() => {
+        api.get('/posts')
+        .then((response) =>{
+            setPost(response.data);
+        })
+    },[])
 
   return(
     <>
     <section className="container">
-        <h1>Mains.Js</h1>
-        <button onClick={handleAxios}>Requisicao</button>
+        <h2 className="mt-3">Articles</h2>
 
         <div className="mt-5 container-posts">
             {
                 post.map(post => {
                     return(
                         <>
-                        <Post key={post.id} subtitle={post.category} title={post.title}></Post>
+                        <Post key={post.id}
+                         subtitle={post.category} 
+                         title={post.title}
+                         author={post.author}
+                         date={post.date}
+                         >
+                            {post.resume}
+                         </Post>
                         </>
                     )
                 })
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,21 +1,25 @@
+
+
 const Post = (props) => {
   return (
-    <div className="post mx-2">
-      <div className="img-post"></div>
-      <h5>{props.subtitle}</h5>
-      <h3>{props.title}</h3>
-      <p>
-        {props.children}
-      </p>
-      <div className="flex pt-2">
-        <div className="img-profile"></div>
-        <div className="desc-profile ml-2">
-          <h6 className="color-blue">Nome</h6>
-          <p>Aug 2, 2024 - 8 min read</p>
-        </div>
+      <div className="post mx-2">
+          <div className="img-post"></div>
+          <h5>{props.subtitle}</h5>
+          <h3>{props.title}</h3>
+          <p className="mt-1">
+              {props.children}
+          </p>
+          <div className="flex pt-2">
+              <div className="img-profile"></div>
+              <div className="desc-profile ml-2">
+                  <h6 className="color-blue">{props.author}</h6>
+                  <p>
+                      {props.date}
+                  </p>
+              </div>
+          </div>
       </div>
-    </div>
   );
-};
+}
 
 export default Post;
